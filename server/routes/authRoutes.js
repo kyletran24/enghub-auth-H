24f89c614071e36199055ed13cdeb637341e0fec
Hiệu sách Hộp <88513815+hieusachhop@users.noreply.github.com>
2024-05-11 23:23:55 +0700

    add production values

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -14,7 +14,7 @@ const {
 router.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://enghub2.onrender.com",
   })
 );
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv").config({ path: "../.env" });
 const cors = require("cors");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
@@ -22,42 +22,6 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", require("./routes/authRoutes"));
-
-// app.get("/api/getAllStudents", async (req, res) => {
-//   try {
-//     const students = await Students.find({});
-//     console.log(students);
-//     res.status(200).json(students);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// app.post("/api/getStudent", async (req, res) => {
-//   try {
-//     const student = await Students.findOne({ stuAcc: req.body.studentAcc });
-//     if (student) {
-//       res.status(200).json(student);
-//     } else {
-//       res.status(401).send("User not found");
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// app.post("/api/getAdmin", async (req, res) => {
-//   try {
-//     const admin = await Admins.findOne({ adminAcc: req.body.adminAcc });
-//     if (admin) {
-//       res.status(200).json(admin);
-//     } else {
-//       res.status(401).send("Admin not found");
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
 
 const PORT = process.env.PORT || 8080;
 
